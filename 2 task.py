76ad6296ea3bbac541d709d45a5a100926bfdea8
Kango911 <lipenkov.a61@gmail.com>
2024-06-20 23:35:05 +0500

    Initial commit

---
+++ 2 task.py
@@ -0,0 +1,16 @@
+# Ввод чисел через Memo (список)
+numbers_input = input("Введите целые числа через пробел: ")
+
+# Преобразование введенных данных в список целых чисел
+numbers = list(map(int, numbers_input.split()))
+
+# Фильтрация чисел, которые не делятся на 5
+filtered_numbers = [num for num in numbers if num % 5 != 0]
+
+# Определение минимального элемента массива
+if filtered_numbers:
+    min_element = min(filtered_numbers)
+    print("Массив, не содержащий числа, делящиеся на 5:", filtered_numbers)
+    print("Минимальный элемент массива:", min_element)
+else:
+    print("Все введенные числа делятся на 5 или массив пуст.")
